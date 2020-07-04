bbc9a2ea633c2952d3bdf6a7e32c10bba6572c3c
selvam16 <vmselvamaj@gmail.com>
2020-07-04 23:37:43 +0530

    Initial impl

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,7 +12,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-
+import Users from 'containers/Users';
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
@@ -20,6 +20,7 @@ export default function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/users" component={Users} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
---
+++ app/containers/Users/component.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Users = () => <h1>User Component</h1>;
+
+export default Users;
